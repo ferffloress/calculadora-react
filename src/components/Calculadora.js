82caeb89202a8dd4dc64d14f7c39d1a55fe8ec81
ferffloress <f.flores016@gmail.com>
2022-12-27 17:31:00 -0300

    calculadora react

---
+++ src/components/Calculadora.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Boton from "./Boton";
+import Pantalla from "./Pantalla";
+import BotonClear from "./BotonClear";
+import { evaluate } from "mathjs";
+
+const Calculadora = () => {
+  const [input, setInput] = useState("");
+
+  const agregarInput = (val) => {
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("Por favor ingrese valores para realizar los cálculos.");
+    }
+  };
+
+  return (
+    <div className="contenedor-calculadora">
+      <Pantalla input={input} />
+      <div className="fila">
+        <Boton manejarClic={agregarInput}>1</Boton>
+        <Boton manejarClic={agregarInput}>2</Boton>
+        <Boton manejarClic={agregarInput}>3</Boton>
+        <Boton manejarClic={agregarInput}>+</Boton>
+      </div>
+      <div className="fila">
+        <Boton manejarClic={agregarInput}>4</Boton>
+        <Boton manejarClic={agregarInput}>5</Boton>
+        <Boton manejarClic={agregarInput}>6</Boton>
+        <Boton manejarClic={agregarInput}>-</Boton>
+      </div>
+      <div className="fila">
+        <Boton manejarClic={agregarInput}>7</Boton>
+        <Boton manejarClic={agregarInput}>8</Boton>
+        <Boton manejarClic={agregarInput}>9</Boton>
+        <Boton manejarClic={agregarInput}>*</Boton>
+      </div>
+      <div className="fila">
+        <Boton manejarClic={calcularResultado}>=</Boton>
+        <Boton manejarClic={agregarInput}>0</Boton>
+        <Boton manejarClic={agregarInput}>.</Boton>
+        <Boton manejarClic={agregarInput}>/</Boton>
+      </div>
+      <div className="fila">
+        <BotonClear manejarClear={() => setInput("")}>C</BotonClear>
+      </div>
+    </div>
+  );
+};
+
+export default Calculadora;
